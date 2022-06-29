8eca44cfbc0da9dbded1f42e4139ef970d9084ef
Alex Keener <alexkeener318@gmail.com>
2022-06-29 02:03:07 -0500

    Creates and sets up starter files

---
+++ script.js
@@ -0,0 +1,18 @@
+function Book(title, author, numPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.read = read;
+    this.info = function() {
+        let output = title + " by " + author + ", " + numPages + " pages, ";
+        if(read){
+            output += "has read it."
+        }else{
+            output += "had not read."
+        }
+        return output;
+    }
+}
+
+let book1 = new Book("12 karrot toothache","post malone", 200, true);
+console.log(book1.info());
